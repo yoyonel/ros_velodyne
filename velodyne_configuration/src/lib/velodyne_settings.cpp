a22f2d8c0b13784173ea8a5e688abb0e2bb3a0ae
ATTY Lionel <lionel.atty@ign.fr>
2016-06-15 18:45:49 +0200

    Travail sur le Velodyne Laser, en particulier sur le package/node de configuration/monitoring. Rajout d'un mécanisme de synchronisation (plus ou moins) fort entre ROS et le VLP. Du coté de ROS (pour l'instant) la synchronisation des settings/status est visible par le Dynamic Server Parameter qui permet de set/get les settings du laser. Inversement du coté du webserver/client du VLP16, il y a une retroaction des settings du DSP-ROS. Cette rétro-action est visible en temps réel dans l'onglet Status du webclient (car rafraichissement périodique (~2hz)), mais ce n'est pas le cas pour l'onglet Settings qui est pensé initialement comme Writer de valeurs par le webclient. Les valeurs visibles dans l'onglet Settings ne sont pas updatées en temps réel, mais un rafraichissement de la page permet d'obtenir leur status courant (updaté). Seulement les 3er paramètres (Laser, Return Type, Motor RPM) sont pris en compte (coté ROS) pour l'instant.

---
+++ velodyne_configuration/src/lib/velodyne_settings.cpp
@@ -0,0 +1,160 @@
+#include <velodyne_settings.h>
+
+
+CVelodyneSettings::CVelodyneSettings() :
+    nh_("~"), /*bAuthSyncROStoLaser_(false),*/ server_(dynamic_reconfigure_mutex_) {
+
+    webserver_.get_ip(nh_);
+
+    //
+    synch_Laser_ROS();
+    //
+//    bAuthSyncROStoLaser_ = true;
+    //
+
+    //----------------------------------------------------
+    // Dynamic Parameter server
+    //----------------------------------------------------
+    server_.setCallback(
+                boost::bind(&CVelodyneSettings::callback, this, _1, _2)
+                );
+    server_.getConfigDefault(config_);
+    //
+    ROS_INFO("Ready to set velodyne settings.\t[DYNAMIC_RECONF]");
+    //----------------------------------------------------
+
+    //----------------------------------------------------
+    // Services
+    //----------------------------------------------------
+    service_ = nh_.advertiseService("get_settings", &CVelodyneSettings::get_settings, this);
+    //
+    ROS_INFO("Ready to get velodyne settings.\t[SERVICE]");
+    //----------------------------------------------------
+
+    //----------------------------------------------------
+    // Publisher
+    //----------------------------------------------------
+    velodyne_settings_pub_ = nh_.advertise<velodyne_configuration::VLP16_SettingsMessage>("velodyne_settings", 10);
+    //
+    ROS_INFO("Publish velodyne settings messages.\t[PUBLISHER]");
+    //----------------------------------------------------
+
+    //----------------------------------------------------
+    // Subscriber
+    //----------------------------------------------------
+    velodyne_settings_sub_ = nh_.subscribe("velodyne_settings", 10,
+                                           &CVelodyneSettings::velodyne_settingsCallback,
+                                           (CVelodyneSettings *) this
+                                           );
+    ROS_INFO("Subscribe to velodyne settings messages.\t[SUBSCRIBER]");
+    //----------------------------------------------------
+}
+
+void CVelodyneSettings::run() {
+    //----------------------------------------
+    // Boucle d'exécution ROS
+    //----------------------------------------
+    //    ros::spin();
+    ros::Rate loop_rate(1);   // 1 Hz
+    while (ros::ok())
+    {
+        if( get_settings(laser_settings_) ) {
+            velodyne_settings_pub_.publish(laser_settings_.msg);
+        }
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+    }
+    //----------------------------------------
+}
+
+bool CVelodyneSettings::get_settings(velodyne_configuration::VLP16_SettingsServiceRequest  &req,
+                                     velodyne_configuration::VLP16_SettingsServiceResponse &res)
+{
+    return get_settings(res);
+}
+
+bool CVelodyneSettings::get_settings(velodyne_configuration::VLP16_SettingsServiceResponse &res)
+{
+    const std::string res_request = webserver_.request_webserver(velodyne_tools::Velodyne_WebServer::WebServerCommands::settings);
+    ROS_INFO_STREAM("Response from VLP webserver: " << res_request );
+
+    return webserver_.parse_JSON_for_settings(res_request, res);
+}
+
+void CVelodyneSettings::callback(velodyne_configuration::VLP16_settingsConfig &config, uint32_t level) {
+//    if( !bAuthSyncROStoLaser_ ) {
+//        ROS_INFO("Failed to synchronize ROS to Laser (non-authorized) !");
+//    }
+//    else {
+    const int return_set_configs = webserver_.send_settings_to_webserver(config);
+    ROS_INFO("Reconfigure Request: %d", return_set_configs);
+//    }
+}
+
+void CVelodyneSettings::velodyne_settingsCallback(velodyne_configuration::VLP16_SettingsMessage _msg)
+{
+    ROS_INFO("velodyne_settingsCallback");
+    synch_Laser_ROS(_msg);
+}
+
+bool CVelodyneSettings::state_has_changed(const velodyne_configuration::VLP16_SettingsMessage& _msg) const
+{
+    return
+            config_.laser_state != _msg.laser_state ||
+            config_.rpm != _msg.rpm ||
+            config_.return_type != atoi(_msg.returns.c_str());
+}
+
+void CVelodyneSettings::update_config(const velodyne_configuration::VLP16_SettingsMessage& _msg)
+{
+    config_.laser_state = _msg.laser_state;
+    config_.rpm = _msg.rpm;
+    config_.return_type = atoi(_msg.returns.c_str());
+}
+
+bool CVelodyneSettings::synch_Laser_ROS(const velodyne_configuration::VLP16_SettingsMessage& _msg)
+{
+    bool ret = true;
+
+    // On verifie qu'il y a eu un changement entre l'état du laser reporté dans le message
+    // et l'état du laser stocké localement dans la config.
+    if( state_has_changed(_msg) ) {
+
+        //------------------------------------------
+        // MAJ de la config (local) du laser
+        //------------------------------------------
+        update_config(_msg);
+
+        //------------------------------------------
+        // MAJ du Dynamic Parameter Server
+        //------------------------------------------
+        // urls:
+        // - http://answers.ros.org/question/57498/notify-changes-to-dynamic_reconfigure/
+        // - http://www.boost.org/doc/libs/1_61_0/libs/thread/example/recursive_mutex.cpp
+        {
+            boost::recursive_mutex::scoped_lock dyn_reconf_lock(dynamic_reconfigure_mutex_);
+            server_.updateConfig(config_);
+        }
+        //------------------------------------------
+    }
+
+    return ret;
+}
+
+bool CVelodyneSettings::synch_Laser_ROS()
+{
+    velodyne_configuration::VLP16_SettingsServiceResponse laser_settings;
+    // On effectue une requete au webserver pour récupérer l'état du laser
+    if( get_settings(laser_settings) ) {
+        // Si on a récupéré l'état,
+        // on met à jour (si besoin) l'enregistrement local (ROS) de l'état du laser
+        return synch_Laser_ROS(laser_settings.msg);
+    }
+    else
+    {
+        ROS_ERROR_STREAM("Can't get settings from VLP-16 webserver !");
+        return false;
+    }
+}
--- velodyne_configuration/src/settings/settings_server.cc
+++ velodyne_configuration/src/settings/settings_server.cc
@@ -1,71 +1,15 @@
 //
 #include <ros/ros.h>
-#include <dynamic_reconfigure/server.h>
 //
-#include "velodyne_configuration/VLP16_settingsConfig.h"
-#include "velodyne_configuration/VLP16_SettingsService.h"
-//
-#include <velodyne_tools.h>
+#include <velodyne_settings.h>
 
-
-velodyne_tools::VLP16_WebServer webserver;
-
-
-/**
- * @brief get_settings
- * @param req
- * @param res
- * @return
- */
-bool get_settings(velodyne_configuration::VLP16_SettingsServiceRequest  &req,
-                  velodyne_configuration::VLP16_SettingsServiceResponse &res)
-{
-    const std::string res_request = webserver.request_webserver(velodyne_tools::Velodyne_WebServer::WebServerCommands::settings);
-    ROS_INFO_STREAM("response from VLP webserver: " << res_request );
-
-    return webserver.parse_JSON_for_settings(res_request, res);
-}
-
-// * @brief callback
-// * @param config
-// * @param level
-// */
-void callback(velodyne_configuration::VLP16_settingsConfig &config, uint32_t level) {
-    const int return_set_configs = webserver.send_settings_to_webserver(config);
-    ROS_INFO("Reconfigure Request: %d", return_set_configs);
-}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "velodyne_settings_server");
 
-    ros::NodeHandle ros_node("~");
-
-    webserver.get_ip(ros_node);
-
-
-    //----------------------------------------------------
-    // Dynamic Parameter server
-    //----------------------------------------------------
-    dynamic_reconfigure::Server<velodyne_configuration::VLP16_settingsConfig> server;
-    dynamic_reconfigure::Server<velodyne_configuration::VLP16_settingsConfig>::CallbackType f;
-
-    f = boost::bind(&callback, _1, _2);
-    server.setCallback(f);
-    //
-    ROS_INFO("Ready to set velodyne settings.");
-    //----------------------------------------------------
-
-
-    //----------------------------------------------------
-    // Services
-    //----------------------------------------------------
-    ros::ServiceServer service = ros_node.advertiseService("get_settings", get_settings);
-    //
-    ROS_INFO("Ready to get velodyne settings.");
-    //----------------------------------------------------
-
-    ros::spin();
+    CVelodyneSettings vs;   // instanciation de l'object (à faire) APRES ros::init !
+    vs.run();
 
     return 0;
 }
